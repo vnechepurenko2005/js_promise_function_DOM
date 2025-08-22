7b4cb37aa887b36f1ecd5ed65aef5e388880d47b
Vladyslav Nechepurenko <fablevlados@gmail.com>
2025-08-23 00:24:51 +0300

    add once true

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,15 @@
 function waitFor(element, eventName) {
   // write your code here
   return new Promise((resolve) => {
-    element.addEventListener(eventName, () => {
-      resolve(
-        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
-      );
-    });
+    element.addEventListener(
+      eventName,
+      () => {
+        resolve(
+          `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+        );
+      },
+      { once: true },
+    );
   });
 }
 
